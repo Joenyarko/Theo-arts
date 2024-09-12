360de7df042cbb4754842927f8fb848dcf127653
Joenyarko <105512053+Joenyarko@users.noreply.github.com>
2024-09-12 17:44:50 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const lightboxDescription = document.getElementById('lightbox-description');
+    const closeBtn = document.querySelector('.close-btn');
+
+    document.querySelectorAll('.view-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            const imgSrc = this.previousElementSibling.src;
+            const imgAlt = this.previousElementSibling.alt;
+            const description = this.getAttribute('data-description'); // Get description from data attribute
+
+            lightboxImg.src = imgSrc;
+            lightboxImg.alt = imgAlt;
+            lightboxDescription.textContent = description; // Set the description
+
+            lightbox.style.display = 'flex';
+        });
+    });
+
+    closeBtn.addEventListener('click', function() {
+        lightbox.style.display = 'none';
+    });
+
+    lightbox.addEventListener('click', function(event) {
+        if (event.target === lightbox) {
+            lightbox.style.display = 'none';
+        }
+    });
+});
+
+
+
